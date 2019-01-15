cf06be809ef962a6ebf5f87e5f062054f1511234
Andrew Curtis <curtis.andrewm@gmail.com>
2019-01-14 16:31:59 -0800

    Add initial index.html content and progress on app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@
     // Days of week in array
     var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     // Number of hours the stores are open each day
-    var operatingHrsDay = 14;
+        // var operatingHrsDay = 14; // Think I'll actually need to loop through open hrs per biz reqs
+    // Create variables to use for accessing elements by ID
+        var firstAndPikeUl = document.getElementById('firstandpike');
+        var seatacUl = document.getElementById('seatac');
+        var seattleCtrUl = document.getElementById('seattlectr');
+        var capHillUl = document.getElementById('caphill');
+        var alkiUl = document.getElementById('alki');
 
 // Create functions that can be reused in method inside each object?
     // Generate number btwn two values (learned from MDN doc on Math.random())
@@ -13,7 +19,8 @@
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min; // The max is exclus and the min is inclus
     }
-    // Calc and store no. of cookies bought per hour based on rand num of customers
+    // Calc and store no. of cookies bought per hour based on rand num of customers and avg no. of cookies/hr sold at a given location
+
 
 // Create separate JS object literal for each shop location
     // Location #1
